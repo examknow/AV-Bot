8191fd8a2522f8bd959af2e9038d484053ca6fb3
examknow <ek311@outlook.com>
2020-05-06 16:12:09 -0500

    add rollback function

--- bot.py
+++ bot.py
@@ -12,7 +12,72 @@ for line in configfile:
     noflagpass = setting[1]
 
 def revertChange(page, user):
+  global username
+  global flagpass
+  global noflagpass
+  S = requests.Session()
 
+  URL = "https://test.miraheze.org/w/api.php"
+
+  PARAMS_1 = {
+      "action": "query",
+      "meta": "tokens",
+      "type": "login",
+      "format": "json"
+  }
+
+  R = S.get(url=URL, params=PARAMS_1)
+  DATA = R.json()
+
+  LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+  PARAMS_2 = {
+      "action": "login",
+      "lgname": username,
+      "lgpassword": noflagpass,
+      "lgtoken": LOGIN_TOKEN,
+      "format": "json"
+  }
+
+  R = S.post(URL, data=PARAMS_2)
+
+  PARAMS_3 = {
+      "action": "query",
+      "meta": "tokens",
+      "format": "json"
+  }
+
+  R = S.get(url=URL, params=PARAMS_3)
+  DATA = R.json()
+
+  CSRF_TOKEN = DATA["query"]["tokens"]["csrftoken"]
+
+  R = S.post(URL, data=PARAMS_4)
+
+  PARAMS_4 = {
+      "action": "query",
+      "meta": "tokens",
+      "type": "rollback",
+      "format": "json"
+  }
+
+  R = S.get(url=URL, params=PARAMS_5)
+  DATA = R.json()
+
+  ROLLBACK_TOKEN = DATA['query']['tokens']['rollbacktoken']
+
+  # Step 5: POST request to rollback a page
+  PARAMS_5 = {
+      "action": "rollback",
+      "format": "json",
+      "title": page,
+      "user": user,
+      "token": ROLLBACK_TOKEN,
+  }
+
+  R = S.post(URL, data=PARAMS_6)
+  DATA = R.json()
+
+  print(DATA)
 
 
 
@@ -37,3 +102,6 @@ for rc in RECENTCHANGES:
     print(str(rc['title']) + ' with the comment: ' + str(rc['comment']))
     if str(rc['comment']) == 'reset':
         print("ALERT! Vandalism found on " + str(rc['title']))
+        rolluser = str(rc['user'])
+        rollpage = str(rc['title'])
+        revertChange(rollpage, rolluser)
